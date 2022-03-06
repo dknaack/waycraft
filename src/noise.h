e6da447a1ce1998f183cd127f901e22435b00be6
Daniel Knaack <daniel.knaack@protonmail.com>
2022-03-06 17:23:41 +0100

    refactor: make noise functions static

--- src/noise.h
+++ src/noise.h
@@ -1,7 +1,7 @@
 #ifndef NOISE_H
 #define NOISE_H
 
-f32 noise_2d(f32 x, f32 y);
-f32 noise_layered_2d(f32 x, f32 y);
+static f32 noise_2d(f32 x, f32 y);
+static f32 noise_layered_2d(f32 x, f32 y);
 
 #endif /* NOISE_H */ 
