941b4a3884e82e773d4375236d887d6c09ab0d96
Daniel Knaack <daniel.knaack@protonmail.com>
2022-03-24 17:03:06 +0100

    wcdb: show different pattern on activate

--- wcdb/main.c
+++ wcdb/main.c
@@ -24,6 +24,9 @@ struct wcdb_client {
     struct wl_surface *surface;
     struct xdg_surface *xdg_surface;
     struct xdg_toplevel *xdg_toplevel;
+
+    u32 *pixels;
+    u8 is_active;
 };
 
 static void
@@ -81,14 +84,14 @@ static void
 xdg_surface_configure(void *data, struct xdg_surface *xdg_surface, u32 serial)
 {
     struct wcdb_client *client = data;
+    i32 width = 256;
+    i32 height = 256;
+    i32 stride = width * 4;
 
     xdg_surface_ack_configure(xdg_surface, serial);
 
     struct wl_buffer *buffer = client->buffer;
     if (!buffer) {
-        i32 width = 256;
-        i32 height = 256;
-        i32 stride = width * 4;
         i32 shm_pool_size = height * stride * 2;
 
         struct wl_shm_pool *pool = client->shm_pool;
@@ -96,18 +99,7 @@ xdg_surface_configure(void *data, struct xdg_surface *xdg_surface, u32 serial)
 
             i32 fd = allocate_shm_file(shm_pool_size);
             i32 prot = PROT_READ | PROT_WRITE;
-            u8 *pool_data = mmap(0, shm_pool_size, prot, MAP_SHARED, fd, 0);
-
-            u32 *pixels = (u32 *)pool_data;
-            for (u32 y = 0; y < height; y++) {
-                for (u32 x = 0; x < width; x++) {
-                    if ((x + y / 8 * 8) % 16 < 8) {
-                        pixels[y * width + x] = 0xff666666;
-                    } else {
-                        pixels[y * width + x] = 0xffeeeeee;
-                    }
-                }
-            }
+            client->pixels = mmap(0, shm_pool_size, prot, MAP_SHARED, fd, 0);
 
             client->shm_pool = pool = wl_shm_create_pool(client->shm, fd, shm_pool_size);
         }
@@ -135,6 +127,28 @@ static const struct xdg_surface_listener xdg_surface_listener = {
 };
 
 static void
+xdg_toplevel_configure(void *data, struct xdg_toplevel *xdg_toplevel,
+                       i32 width, i32 height, struct wl_array *states)
+{
+    struct wcdb_client *client = data;
+    i32 is_active = 0;
+    i32 *state;
+    wl_array_for_each(state, states) {
+        if (*state == XDG_TOPLEVEL_STATE_ACTIVATED) {
+            is_active = 1;
+        }
+    }
+
+    client->is_active = is_active;
+}
+
+static const struct xdg_toplevel_listener xdg_toplevel_listener = {
+    .configure        = xdg_toplevel_configure,
+    .close            = do_nothing,
+    .configure_bounds = do_nothing,
+};
+
+static void
 xdg_wm_base_ping(void *data, struct xdg_wm_base *xdg_wm_base, u32 serial)
 {
     xdg_wm_base_pong(xdg_wm_base, serial);
@@ -142,6 +156,42 @@ xdg_wm_base_ping(void *data, struct xdg_wm_base *xdg_wm_base, u32 serial)
 
 static const struct xdg_wm_base_listener xdg_wm_base_listener = {
     .ping = xdg_wm_base_ping,
+};
+
+static const struct wl_callback_listener wl_surface_frame_listener;
+
+static void
+wl_surface_frame_done(void *data, struct wl_callback *cb, u32 time)
+{
+    struct wcdb_client *client = data;
+
+    wl_callback_destroy(cb);
+    cb = wl_surface_frame(client->surface);
+    wl_callback_add_listener(cb, &wl_surface_frame_listener, client);
+
+    i32 height = 256;
+    i32 width = 256;
+
+    u32 *pixels = client->pixels;
+    u32 white = client->is_active ? 0xff666666 : 0xffeeeeee;
+    u32 black = client->is_active ? 0xffeeeeee : 0xff666666;
+    for (u32 y = 0; y < height; y++) {
+        for (u32 x = 0; x < width; x++) {
+            if ((x + y / 8 * 8) % 16 < 8) {
+                pixels[y * width + x] = black;
+            } else {
+                pixels[y * width + x] = white;
+            }
+        }
+    }
+
+    struct wl_surface *surface = client->surface;
+    wl_surface_damage(surface, 0, 0, UINT32_MAX, UINT32_MAX);
+    wl_surface_commit(surface);
+}
+
+static const struct wl_callback_listener wl_surface_frame_listener = {
+    .done = wl_surface_frame_done,
 };
 
 static void
@@ -192,8 +242,12 @@ main(void)
     client.xdg_surface = xdg_wm_base_get_xdg_surface(client.xdg_wm_base, client.surface);
     xdg_surface_add_listener(client.xdg_surface, &xdg_surface_listener, &client);
     client.xdg_toplevel = xdg_surface_get_toplevel(client.xdg_surface);
+    xdg_toplevel_add_listener(client.xdg_toplevel, &xdg_toplevel_listener, &client);
     xdg_toplevel_set_title(client.xdg_toplevel, "wcdb");
     wl_surface_commit(client.surface);
+
+    struct wl_callback *cb = wl_surface_frame(client.surface);
+    wl_callback_add_listener(cb, &wl_surface_frame_listener, &client);
 
     while (wl_display_dispatch(client.display)) {
 
