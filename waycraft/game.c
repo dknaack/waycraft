9adfd65e73f96209286486eff6df3142d1cfaf28
Daniel Knaack <daniel.knaack@protonmail.com>
2022-03-30 11:26:23 +0200

    game: fix rendering order of world and windows
    Windows are rendered last since they may be transparent. This way the
    windows don't cut through the world anymore.

--- waycraft/game.c
+++ waycraft/game.c
@@ -602,10 +602,8 @@ game_update(struct backend_memory *memory, struct game_input *input,
 	}
 
 	world_update(&game->world, game->camera.position, render_commands);
-
-	window_render(windows, window_count, render_commands);
 	world_render(&game->world, render_commands);
-
+	window_render(windows, window_count, render_commands);
 	renderer_end_frame(&game->renderer, render_commands);
 
 	gl.UseProgram(game->renderer.shader.program);
