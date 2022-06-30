dab41b39dd43325b6946e88cb5222a75ad08c72f
Daniel Knaack <daniel.knaack@protonmail.com>
2022-06-30 20:10:11 +0200

    merge inventory files with game

--- waycraft/game.c
+++ waycraft/game.c
@@ -14,7 +14,6 @@
 #include "waycraft/renderer.c"
 #include "waycraft/block.c"
 #include "waycraft/debug.c"
-#include "waycraft/inventory.c"
 #include "waycraft/noise.c"
 #include "waycraft/timer.c"
 #include "waycraft/world.c"
@@ -64,6 +63,173 @@ get_texture(struct game_assets *assets, u32 texture_id)
 	return assets->textures[texture_id];
 }
 
+// NOTE: the direction determines the direction that the block faces after
+// placing it in the world.
+static enum block_type
+item_to_block(enum item_type item, v3 direction)
+{
+	if (ITEM_FIRST_BLOCK < item && item < ITEM_LAST_BLOCK) {
+		enum block_type block = item - (ITEM_FIRST_BLOCK + 1);
+
+		u32 can_rotate = item == ITEM_MONITOR_BLOCK;
+		if (can_rotate) {
+			v3 abs_direction = v3_abs(direction);
+
+			u32 is_facing_x_axis = abs_direction.x > abs_direction.y &&
+				abs_direction.x > abs_direction.z;
+			u32 is_facing_y_axis = abs_direction.y > abs_direction.x &&
+				abs_direction.y > abs_direction.z;
+
+			u32 is_facing_in_negative_direction = 0;
+			if (is_facing_x_axis) {
+				is_facing_in_negative_direction = direction.x < 0;
+			} else if (is_facing_y_axis) {
+				is_facing_in_negative_direction = direction.y < 0;
+				block += 2;
+			} else {
+				is_facing_in_negative_direction = direction.z < 0;
+				block += 4;
+			}
+
+			block += is_facing_in_negative_direction;
+		}
+
+		return block;
+	} else {
+		return 0;
+	}
+}
+
+static void
+item_render(struct inventory_item *item, struct rectangle rect,
+		struct render_command_buffer *cmd_buffer, struct game_assets *assets)
+{
+	struct texture_id texture = get_texture(assets, TEXTURE_BLOCK_ATLAS).id;
+
+	if (item->type == ITEM_WINDOW) {
+		v3 pos0 = V3(rect.x + 1 * rect.width, rect.y + 1 * rect.height, 0);
+		v3 pos1 = V3(rect.x + 0 * rect.width, rect.y + 1 * rect.height, 0);
+		v3 pos2 = V3(rect.x + 1 * rect.width, rect.y + 0 * rect.height, 0);
+		v3 pos3 = V3(rect.x + 0 * rect.width, rect.y + 0 * rect.height, 0);
+
+		v2 uv[4] = {0};
+		block_texcoords_right(BLOCK_AIR, uv);
+		render_quad(cmd_buffer, pos0, pos1, pos2, pos3,
+			uv[0], uv[1], uv[2], uv[3], texture);
+	} else {
+		enum block_type block = item_to_block(item->type, V3(1, 0, 0));
+		if (block != BLOCK_AIR) {
+			v3 pos0 = V3(rect.x + 1 * rect.width, rect.y + 1 * rect.height, 0);
+			v3 pos1 = V3(rect.x + 0 * rect.width, rect.y + 1 * rect.height, 0);
+			v3 pos2 = V3(rect.x + 1 * rect.width, rect.y + 0 * rect.height, 0);
+			v3 pos3 = V3(rect.x + 0 * rect.width, rect.y + 0 * rect.height, 0);
+
+			v2 uv[4] = {0};
+			block_texcoords_right(block, uv);
+			render_quad(cmd_buffer, pos0, pos1, pos2, pos3,
+				uv[0], uv[1], uv[2], uv[3], texture);
+		}
+	}
+}
+
+static void
+inventory_render(struct inventory *inventory,
+		f32 screen_width, f32 screen_height,
+		struct render_command_buffer *cmd_buffer,
+		struct game_assets *assets)
+{
+	struct texture hotbar_texture = get_texture(assets, TEXTURE_HOTBAR);
+	f32 hotbar_aspect = hotbar_texture.width / hotbar_texture.height;
+
+	struct rectangle hotbar = {0};
+	hotbar.width = screen_width * 0.5f;
+	hotbar.height = hotbar.width / hotbar_aspect;
+	hotbar.x = (screen_width - hotbar.width) / 2.0f;
+	hotbar.y = 0;
+
+	render_sprite(cmd_buffer, hotbar, hotbar_texture.id);
+
+	if (inventory->is_active) {
+		struct texture_id texture = get_texture(assets, TEXTURE_INVENTORY).id;
+		m4x4 transform = m4x4_id(0.75);
+		render_textured_quad(cmd_buffer, transform, texture);
+
+		//struct inventory_item *item = inventory->items;
+		u32 y = LENGTH(inventory->items) / 9;
+		while (y-- > 0) {
+			for (u32 x = 0; x < 9; x++) {
+				//item_render(item++, x, y, cmd_buffer);
+			}
+		}
+	} else {
+		f32 slot_size = hotbar.height / 20.0 * 14.0;
+		f32 slot_advance = hotbar.height / 20.0f * 17.5f;
+
+		struct rectangle active_slot = {0};
+		active_slot.x = hotbar.x + slot_advance * inventory->active_item;
+		active_slot.y = hotbar.y;
+		active_slot.width = hotbar.height;
+		active_slot.height = hotbar.height;
+
+		struct texture_id texture = get_texture(assets, TEXTURE_ACTIVE_SLOT).id;
+		render_sprite(cmd_buffer, active_slot, texture);
+
+		struct inventory_item *item = inventory->items;
+		for (u32 i = 0; i < 9; i++) {
+			struct rectangle item_rect = {0};
+			item_rect.x = hotbar.x + 0.5f * (hotbar.height - slot_size) + slot_advance * i;
+			item_rect.y = hotbar.y + 0.5f * (hotbar.height - slot_size);
+			item_rect.width = slot_size;
+			item_rect.height = slot_size;
+
+			item_render(item++, item_rect, cmd_buffer, assets);
+		}
+	}
+}
+
+static u32
+item_stack_size(enum item_type item)
+{
+	switch (item) {
+	case ITEM_WINDOW:
+		return 0;
+	default:
+		return 64;
+	}
+}
+
+static void
+inventory_add_item(struct inventory *inventory, enum item_type item, u32 count)
+{
+	struct inventory_item *items = inventory->items;
+
+	u32 max_stack_size = item_stack_size(item);
+	for (u32 i = 0; i < LENGTH(inventory->items); i++) {
+		enum item_type item_type = items[i].type;
+		if (item_type == 0 || item_type == item) {
+			u32 item_count = items[i].count;
+			assert(item_type == 0 ? item_count == 0 : 1);
+
+			bool is_empty = item_count == 0;
+			bool is_non_stackable = max_stack_size == 0;
+			if (is_non_stackable && is_empty) {
+				items[i].type = item;
+				items[i].count = count;
+				break;
+			} else if (is_empty || item_count < max_stack_size) {
+				items[i].type = item;
+				items[i].count = MAX(item_count + count, max_stack_size);
+				if (item_count + count < max_stack_size) {
+					break;
+				} else {
+					count -= max_stack_size - item_count;
+				}
+			}
+		}
+
+		item++;
+	}
+}
 static void
 camera_init(struct camera *camera, v3 position, f32 fov)
 {
--- waycraft/game.h
+++ waycraft/game.h
@@ -4,7 +4,6 @@
 #include <waycraft/block.h>
 #include <waycraft/debug.h>
 #include <waycraft/gl.h>
-#include <waycraft/inventory.h>
 #include <waycraft/log.h>
 #include <waycraft/memory.h>
 #include <waycraft/renderer.h>
@@ -18,6 +17,40 @@ struct camera {
 	f32 pitch;
 	f32 fov;
 	v2 viewport;
+};
+
+enum item_type {
+	ITEM_NONE,
+	// NOTE: blocks should be synchronized with the block_type enum.
+	ITEM_FIRST_BLOCK = ITEM_NONE,
+    ITEM_AIR_BLOCK,
+    ITEM_STONE_BLOCK,
+    ITEM_DIRT_BLOCK,
+    ITEM_GRASS_BLOCK,
+    ITEM_GRASS_TOP_BLOCK,
+    ITEM_PLANKS_BLOCK,
+    ITEM_OAK_LOG_BLOCK,
+    ITEM_OAK_LEAVES_BLOCK,
+    ITEM_SAND_BLOCK,
+    ITEM_WATER_BLOCK,
+    ITEM_MONITOR_BLOCK,
+	ITEM_LAST_BLOCK,
+    ITEM_WINDOW = ITEM_LAST_BLOCK,
+	ITEM_COUNT
+};
+
+struct inventory_item {
+	enum item_type type;
+	union {
+		u32 id;
+		u32 count;
+	};
+};
+
+struct inventory {
+	struct inventory_item items[36];
+	u8 active_item;
+	bool is_active;
 };
 
 struct player {
