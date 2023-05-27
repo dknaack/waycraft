939db8a924ebeda699c7dbb7abd27b4158a035ee
Daniel Knaack <daniel.knaack@protonmail.com>
2023-05-27 10:39:52 +0200

    add usz, isz types

--- waycraft/types.h
+++ waycraft/types.h
@@ -2,13 +2,13 @@
 #include <stddef.h>
 #include <stdbool.h>
 
-typedef uintptr_t usize;
+typedef uintptr_t usize, usz;
 typedef uint64_t  u64;
 typedef uint32_t  u32;
 typedef uint16_t  u16;
 typedef uint8_t   u8;
 
-typedef intptr_t isize;
+typedef intptr_t isize, isz;
 typedef int64_t  i64;
 typedef int32_t  i32;
 typedef int16_t  i16;
